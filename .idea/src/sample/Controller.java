8df94169b5a1897003b2a384b2bfd9caf5dd3197
Louis-sch <louis.schibler@gmail.com>
2020-10-18 15:24:39 +0200

    Config JavaFx

---
+++ .idea/src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void LoadMap(ActionEvent event) {
+        System.out.println("Contresens, ma chérie ti es a contresens ");
+    }
+
+}
---
+++ .idea/src/sample/Main.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage MainFrame) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        MainFrame.setTitle("Kante");
+
+        MainFrame.setScene(new Scene(root, 1920, 1080));
+        MainFrame.setResizable(false);
+        MainFrame.show();
+
+        Group map = new Group();
+        Line line = new Line(40,100,10,400);
+        map.getChildren().add(line);
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
