ec3f95e8faf18dab054ddc9161633f03dc962ca6
Armandleullier <armand.leullier@gmail.com>
2020-10-18 21:41:06 +0200

    addJUnit to Project

---
+++ test/view/WindowControlTest.java
@@ -0,0 +1,16 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WindowControlTest {
+
+    @Test
+    void loadMap() {
+    }
+
+    @Test
+    void drawLines() {
+    }
+}
---
+++ test/view/WindowTest.java
@@ -0,0 +1,16 @@
+package view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WindowTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void main() {
+    }
+}
