8be0ead8401e0c517ca07e89cf8eec71b9602e52
jkritter <48760362+jkritter@users.noreply.github.com>
2020-10-19 11:46:18 +0200

    Cablage Model

--- src/model/Path.java
+++ src/model/Path.java
@@ -10,10 +10,34 @@ public class Path {
      *
      */
     protected int pathLength;
+    protected List<Segment> listSegments;
 
     /**
      * Default constructor
      */
     public Path() {
+    }
+
+    public Path(List<Segment> listSegments) {
+        this.listSegments = listSegments;
+        this.pathLength = 0;
+        for(Segment s: listSegments) {
+            this.pathLength += s.getLength();
+        }
+    }
+
+    /**
+     * Getters - Setters
+     */
+    public int getPathLength() {
+        return pathLength;
+    }
+
+    public List<Segment> getListSegments() {
+        return listSegments;
+    }
+
+    public int getDuration() {
+        return (int) (((double) pathLength/15.0)*3.6);
     }
 }
--- src/model/Request.java
+++ src/model/Request.java
@@ -10,10 +10,54 @@ public class Request extends Observable {
      *
      */
     protected int order;
+    protected PickUpPoint pickUpPoint;
+    protected DeliveryPoint deliveryPoint;
 
     /**
      * Default constructor
      */
     public Request() {
+    }
+
+    public Request(int order, PickUpPoint pickUpPoint, DeliveryPoint deliveryPoint) {
+        this.order = order;
+        this.pickUpPoint = pickUpPoint;
+        this.deliveryPoint = deliveryPoint;
+    }
+
+    /**
+     * Getters - Setters
+     */
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public PickUpPoint getPickUpPoint() {
+        return pickUpPoint;
+    }
+
+    public void setPickUpPoint(PickUpPoint pickUppoint) {
+        this.pickUpPoint = pickUppoint;
+    }
+
+    public DeliveryPoint getDeliveryPoint() {
+        return deliveryPoint;
+    }
+
+    public void setDeliveryPoint(DeliveryPoint deliveryPoint) {
+        this.deliveryPoint = deliveryPoint;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "order=" + order +
+                ", pickUpPoint=" + pickUpPoint +
+                ", deliveryPoint=" + deliveryPoint +
+                '}';
     }
 }
