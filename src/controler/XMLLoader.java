de1ed24785d4d13f34232df3fbd0d99337da76ee
Lucas Tissier <lucastissier51@gmail.com>
2020-10-19 11:22:18 +0200

    Request fil parsing in progress

--- src/controler/XMLLoader.java
+++ src/controler/XMLLoader.java
@@ -10,6 +10,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import model.Request;
 import model.Segment;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
@@ -87,4 +88,64 @@ public class XMLLoader {
         mapParsed.display();
         return mapParsed;
     }
+
+    /*public void parseRequestXML(String pathNameXMLFile, Map map){
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        ArrayList<Request> listRequest = new ArrayList<Request>();
+
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            File fileXML = new File(pathNameXMLFile);
+            Document xml;
+
+            xml = builder.parse(fileXML);
+            Element root = xml.getDocumentElement();
+
+            NodeList nodes = root.getChildNodes();
+            int nbNode = nodes.getLength();
+            for(int i = 0; i < nbNode; i++){
+                Node n = nodes.item(i);
+                String nodeName = n.getNodeName();
+
+                if(n.getAttributes() != null && n.getAttributes().getLength() > 0){
+
+                    NamedNodeMap att = n.getAttributes();
+                    int nbAtt = att.getLength();
+
+                    if(nodeName == "intersection")
+                    {
+                        long id = Long.parseLong(att.item(0).getNodeValue());
+                        double latitude = Double.parseDouble(att.item(1).getNodeValue());
+                        double longitude = Double.parseDouble(att.item(2).getNodeValue());
+                        Intersection intersection = new Intersection(id, latitude, longitude);
+                        listIntersection.add(intersection);
+                    }
+
+                    if(nodeName == "segment")
+                    {
+                        long destination = Long.parseLong(att.item(0).getNodeValue());
+                        double length = Double.parseDouble(att.item(1).getNodeValue());
+                        String name = att.item(2).getNodeValue();
+                        long origin = Long.parseLong(att.item(3).getNodeValue());
+                        Segment segment = new Segment(destination, length, name, origin);
+                        listSegment.add(segment);
+                    }
+                }
+            }
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Map mapParsed = new Map(listIntersection, listSegment);
+        System.out.println("Map créée");
+        mapParsed.display();
+    }*/
+
+
 }
